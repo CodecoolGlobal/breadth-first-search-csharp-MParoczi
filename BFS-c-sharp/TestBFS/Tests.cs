951f7fcfb0999c978a088973234d992cff84ab46
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-12 17:27:10 +0100

    Implement method "GenerateTestGraph" in Tests.cs

--- BFS-c-sharp/TestBFS/Tests.cs
+++ BFS-c-sharp/TestBFS/Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using BFS_c_sharp.Model;
 using NUnit.Framework;
 
 namespace TestBFS
@@ -6,6 +8,38 @@ namespace TestBFS
     [TestFixture]
     public class Tests
     {
+        private List<UserNode> GenerateTestGraph()
+        {
+            List<UserNode> users = new List<UserNode>();
+            
+            UserNode a = new UserNode("Node", "A", Guid.NewGuid().ToString());
+            UserNode b = new UserNode("Node", "B", Guid.NewGuid().ToString());
+            UserNode c = new UserNode("Node", "C", Guid.NewGuid().ToString());
+            UserNode d = new UserNode("Node", "D", Guid.NewGuid().ToString());
+            UserNode e = new UserNode("Node", "E", Guid.NewGuid().ToString());
+
+            a.Friends.Add(b);
+            a.Friends.Add(c);
+            a.Friends.Add(e);
+
+            b.Friends.Add(a);
+            b.Friends.Add(e);
+
+            c.Friends.Add(a);
+            c.Friends.Add(d);
+
+            d.Friends.Add(c);
+            d.Friends.Add(e);
+
+            e.Friends.Add(a);
+            e.Friends.Add(b);
+            e.Friends.Add(d);
+            
+            users.AddRange(new []{a, b, c, d, e});
+
+            return users;
+        }
+
         [Test]
         public void Test1()
         {
