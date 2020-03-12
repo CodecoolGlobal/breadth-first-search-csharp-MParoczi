57cf88ae0dd8980636fd8af12216505a99570e95
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-12 16:48:12 +0100

    Implement method "GetDistanceBetweenUsers"

--- BFS-c-sharp/BreadthFirstSearch.cs
+++ BFS-c-sharp/BreadthFirstSearch.cs
@@ -43,7 +43,34 @@ namespace BFS_c_sharp
 
         public int GetDistanceBetweenTwoUsers(UserNode userOne, UserNode userTwo)
         {
-            throw new NotImplementedException();
+            if (userOne.Equals(userTwo))
+            {
+                return 0;
+            }
+            
+            List<UserNode> visitedUsers = new List<UserNode>();
+            Queue<UserNode> usersToVisit = new Queue<UserNode>();
+            int iteration = 0;
+            
+            usersToVisit.Enqueue(userOne);
+
+            while (usersToVisit.Count != 0)
+            {
+                iteration += 1;
+                
+                UserNode currentUser = usersToVisit.Dequeue();
+                
+                visitedUsers.Add(currentUser);
+
+                foreach (UserNode friend in currentUser.Friends)
+                {
+                    if (visitedUsers.Contains(friend) || usersToVisit.Contains(friend)) continue;
+                    if (friend.Id.Equals(userTwo.Id)) return iteration;
+                    usersToVisit.Enqueue(friend);
+                }
+            }
+
+            return iteration;
         }
 
         public List<UserNode> GetFriendsInDistance(UserNode user, int distance)
