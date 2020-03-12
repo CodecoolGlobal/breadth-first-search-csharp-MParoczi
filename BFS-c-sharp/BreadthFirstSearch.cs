cb68675f69660d8d9e9ff45988185c69aed7f092
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-12 14:53:31 +0100

    Add constructor to class "BreadthFirstSearch"

--- BFS-c-sharp/BreadthFirstSearch.cs
+++ BFS-c-sharp/BreadthFirstSearch.cs
@@ -1,7 +1,15 @@
-﻿namespace BFS_c_sharp
+﻿using System.Collections.Generic;
+using BFS_c_sharp.Model;
+
+namespace BFS_c_sharp
 {
     public class BreadthFirstSearch
     {
-        
+        private readonly List<UserNode> _graph;
+
+        public BreadthFirstSearch(List<UserNode> graph)
+        {
+            _graph = graph;
+        }
     }
 }
